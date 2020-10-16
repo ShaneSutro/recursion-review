1e350d830c04f32b9210a3d5c44b7371bbb33627
Shane Sutro <shanesutro@gmail.com>
2020-10-15 20:36:25 -0600

    Complete Bare Minimum Requirements

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,27 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+
+// must use document.body
+// element.childNodes
+// element.classList
+
+var getElementsByClassName = function(className, node) {
   // your code here
+  var result = [];
+  var node = node || document.body;
+
+  if (node.classList) {
+    if (node.classList.value.indexOf(className) !== -1) {
+      result.push(node);
+    }
+  }
+
+  if (node.childNodes) {
+    for (let i = 0; i < node.childNodes.length; i++) {
+      result = result.concat(getElementsByClassName(className, node.childNodes[i]));
+    }
+  }
+
+  return result;
 };
