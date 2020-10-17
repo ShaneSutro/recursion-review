3b138f51a98188a0ba705d9d56c0b09a22800cc6
Shane Sutro <shanesutro@gmail.com>
2020-10-17 13:35:20 -0600

    Added logic for Boolean and Null data types

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -36,11 +36,11 @@
 // value
 //     string ---
 //     number ---
-//     object
+//     object {
 //     array
-//     true
-//     false
-//     null
+//     true ---
+//     false ---
+//     null ---
 
 // if  Number(value).charCode() >= 85 || Number(value).charCode() <=288
 
@@ -60,14 +60,21 @@ var parseJSON = function(json) {
     return json[index];
   };
 
-  //check for types
+  // check for types ----------------------------------------
+
   var nextChar = getNextChar(json);
 
   if (nextChar === '"') {
     return parseString(getNextChar(json));
-  } else if (nextChar.charCodeAt() >= 48 || nextChar.charCodeAt() <= 57) {
+  } else if (nextChar.charCodeAt() >= 48 && nextChar.charCodeAt() <= 57) {
     return parseNumber(nextChar);
+  } else if (nextChar === 't' || nextChar === 'f') {
+    return parseBoolean(nextChar);
+  } else if (nextChar === 'n') {
+    return parseNull();
   }
+
+  // helper functions for types -----------------------------
 
   var parseString = function(char) {
     if (char === '"') {
@@ -83,6 +90,31 @@ var parseJSON = function(json) {
     }
 
     return Number(char + parseNumber(getNextChar(json)));
+  };
+
+  var parseBoolean = function(char) {
+    var endIndex;
+    if (char === 't') {
+      endIndex = index + 4;
+    } else {
+      endIndex = index + 5;
+    }
+
+    let boolean = json.slice(index, endIndex);
+    if (boolean === 'true') {
+      index += 4;
+      return true;
+    } else if (boolean === 'false') {
+      index += 5;
+      return false;
+    }
+  };
+
+  var parseNull = function() {
+    if (json.slice(index, index + 4) === 'null') {
+      index += 4;
+      return null;
+    }
   };
 
 };
